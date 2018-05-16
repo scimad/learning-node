c8008db57d825e8d129adf1bde78ad9f485ccb01
scimad <scimad2.71@gmail.com>
2018-05-16 23:27:15 +0545

    module & exports

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const http = require('http');
+
+const fn = require('./fn_exprn');
+
 const hostname = 'localhost';
 const port = 3000;
+
+
+fn.add(10,20);
+fn.callfn(fn.sayBye,'Everyone');
+
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -8,6 +16,10 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+
+
--- fn_exprn.js
+++ fn_exprn.js
@@ -6,10 +6,23 @@ function sayHello(user){
 function callFunction(funct,param){
     funct(param);
 }
+
 var sayBye = function(username){
     console.log('Bye Bye', username, ' :(');
 }
-//sayBye('Sparrow');
-callFunction(sayBye,"Kitty");
 
-module.exports = callFunction;      //allows to use the 'callFunction' in other modules
+//sayBye('Sparrow');
+//callFunction(sayBye,"Kitty");
+
+
+var adder = function(a,b){
+    console.log(`The sum is:${a+b}`);
+}
+
+var lang = 'en-us';
+
+module.exports.callfn = callFunction;      //allows to use the 'callFunction' in other modules
+module.exports.add = adder;
+module.exports.lang = lang;
+module.exports.sayBye = sayBye;
+module.exports.sayHello = sayHello;
