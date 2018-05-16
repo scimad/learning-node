936f21062a4044be6baf7de12602a64817bef794
scimad <scimad2.71@gmail.com>
2018-05-16 23:56:38 +0545

    more on listener & emitter

--- events2.js
+++ events2.js
@@ -1,4 +1,5 @@
 var events = require('events');
+var util = require('util');
 
 var myEmitter = new events.EventEmitter();
 myEmitter.on('error', function(code){       //registering an event listener for error
@@ -6,3 +7,23 @@ myEmitter.on('error', function(code){       //registering an event listener for 
 });
 
 myEmitter.emit('error','404');              //emitting the event 'error'
+
+var Person = function(abc){
+    this.name = abc;
+}
+
+util.inherits(Person, events.EventEmitter); //object created using Persion() constructor inherits EventEmitter 
+
+var b4 = new Person('Bichar');
+var scimad = new Person ('Madhav');
+var ram = new Person('Ramanujan');
+
+var people = [b4, scimad, ram];
+
+people.forEach(function(someone){
+    someone.on('speak',function(msg){
+        console.log(`${someone.name} said ${msg}.`);
+    });
+});
+
+ram.emit('speak','universe is great');
