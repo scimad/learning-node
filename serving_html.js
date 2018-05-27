d9e122be9f1ec45cbd115b6dbc83d958a29b35bd
scimad <scimad2.71@gmail.com>
2018-05-28 00:14:45 +0545

    html page served

---
+++ serving_html.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+function onRequest(request, response){
+    console.log(request.url);
+    response.writeHead(200, {"Content-Type":"text/html"});
+    
+    var myReadStream = fs.createReadStream('./home.html','utf8');
+
+    myReadStream.pipe(response);
+}
+
+var server = http.createServer(onRequest);
+
+server.listen(3000); //server.listen(3000,'127.0.0.1'); 
+console.log('server started @ port 3000 using pipe');
